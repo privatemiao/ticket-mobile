0a4a91f7de23857aa6b588f4d434f1cabb8075c1
Mel Liu <79642742@qq.com>
2015-10-15 17:19:25 +0800

    a

--- ticket/www/js/controllers/BookingController.js
+++ ticket/www/js/controllers/BookingController.js
@@ -2,5 +2,21 @@ angular.module('ticket.controllers').controller("BookingController", function($i
 	$ionicPlatform.ready(function() {
 		$scope.title = 'Booking...';
 		BookingService.echo();
+
+		$scope.datepickerObject = {
+			inputDate : new Date(), // Optional
+			mondayFirst : true, // Optional
+			showTodayButton : 'true', // Optional
+			modalHeaderColor : 'bar-positive', // Optional
+			modalFooterColor : 'bar-positive', // Optional
+			from : new Date(2012, 8, 2), // Optional
+			to : new Date(2018, 8, 25), // Optional
+			callback : function(val) { // Mandatory
+				this.inputDate = val;
+			}
+		};
 	});
+
+	//
+	//
 });
